a6273ed10dc06d3842e3aa0ac92522437378612e
Kckbn <jan-dahlke@gmx.de>
2023-05-19 14:00:48 +0200

    first version of docstring

--- Board.py
+++ Board.py
@@ -9,34 +9,48 @@ class Board:
     field = None 
 
     def __init__(self, m = 5, n = 5,  k = 4) -> None:
+        '''
+        This is the constructor for the class Board. It sets its number of columns and rows and the length of the row needed to win.
+
+                Parameters:
+                        m(int): Board's number of columns
+                        n(int): Board's number of rows
+                        k(int): length of the row needed to win         
+        '''
         self.nRow = n
         self.mCol = m
         self.kInARow = k 
         self.field = np.full([m, n], 0)
     
     def reset(self) -> None:
+        """Resets the board's fields to default (0)."""
         self.field.fill(0)
         
 
     def fillUp(self, number: int) -> None:
+        """Fills the Board's fields with"""
         self.field.fill(number)
 
     def createTopBorder(self) -> str:
+        """Returns a string that displays the board's columns."""
         topBorder = "  | "
         for i in range(len(self.field.tolist())):
             topBorder += str(i) + " | "
         return(topBorder)
 
     def createBoardRow(self, rowIndex: int) -> str:
+        """Returns a string that displays the board's rows including the field itself."""
         row = str(rowIndex) + " | "
         for j in range(len(self.field.tolist()[rowIndex])):
             row += self.visualizeNumber(self.field.tolist()[rowIndex][j]) + " | "
         return(row)
 
     def createBorder(self) -> str:
+        """Returns a string that is a dotted line, matching the board's length."""
         return((len(self.createBoardRow(0)) - 1) * "-")
 
     def display(self) -> None:
+        """Returns the Board as a complete string."""
         field_list = self.field.tolist()        
         #Information
         print(self.toString())
@@ -52,6 +66,17 @@ class Board:
         
 
     def visualizeNumber(self, number: int) -> str:
+        '''
+        Visualizes player_number to either O, X or simple gap. Returns a string.
+
+                Parameters:
+                        number(int): Number to visualize
+
+                Returns:
+                        if number is 1, "O" is returned
+                        if number is 2, "X" is returned
+                        else " " is returned
+        '''
         if number == 1:
             return "O"
         elif number == 2:
@@ -60,6 +85,16 @@ class Board:
             return " "
 
     def positionFree(self, row: int, col: int) -> bool:
+        '''
+        Checks if Position is free. Returns boolean
+
+                Parameters:
+                        row(int): Row of the field to check
+                        col(int): Column of the field to check
+
+                Returns: 
+                        Returns if field is free as a boolean
+        '''
         if self.field[row, col] == 0:
             print("position free")
             return True
@@ -68,6 +103,16 @@ class Board:
             return False
 
     def positionExists(self, row:int, col:int) -> bool:
+        '''
+        Checks if Position exists. Returns boolean
+
+                Parameters:
+                        row(int): Row of the field to check
+                        col(int): Column of the field to check
+
+                Returns: 
+                        Returns if field exists as a boolean
+        '''
         if self.nRow <= row or row < 0 or self.mCol <= col or col < 0:
             print("position doesnt exist")
             return False
@@ -82,12 +127,13 @@ class Board:
     
 
     def toString(self) -> str:
+        """Returns the number of columns and rows and the needed span of fields to win."""
         return ("mCol: " + str(self.mCol) + '\n'+ "nRow: " + str(self.nRow) + '\n' + "kInRow: " + str(self.kInARow) )
 
-board = Board()
-board.fillUp(2)
-board.display()
-board.reset()
-board.display()
-board.fillUp(2)
-board.display()
+#board = Board()
+#board.fillUp(2)
+#board.display()
+#board.reset()
+#board.display()
+#board.fillUp(2)
+#board.display()
--- Player.py
+++ Player.py
@@ -3,11 +3,31 @@ import numpy as np
 
 class Player:
     def __init__(self, name: str, player_number: int, board: Board ) -> None:
+        '''
+        This is the constructor for the class Player. It sets its name, player_number and Board.
+
+                Parameters:
+                        name(string): player's name
+                        player_name(int): player's number
+                        board(Board): Board where Player is active
+        '''
         self.player_number = player_number
         self.name = name
         self.board = board
 
     def make_move(self, row: int, col: int, board:Board) -> tuple:
+        '''
+        Returns if existing and free the field. Sets the field to the given player_number.
+
+                Parameters:
+                        self: player
+                        row(int): field's row
+                        col(int): field's column
+                        board(Board): board where field is set
+
+                Returns:
+                        tuple: field coordinates
+        '''
         if board.positionExists(row, col) and board.positionFree(row, col):          
             print("made a move")
             board.field[row, col] = self.player_number
@@ -16,6 +36,7 @@ class Player:
 
     
     def toString(self) -> str:
+        """Returns name and the number of a player as a string."""
         return("Name: " + self.name + '\n' + "Player Number: " + str(self.player_number))#
 #b = Board(5, 5)
 #p1 = Player("name", 1, b)
