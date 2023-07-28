34edba446a77bacda852b02a0c9fcbadc25696be
Kckbn <jan-dahlke@gmx.de>
2023-07-28 23:24:38 +0200

    mybot documentation

--- MyBot.py
+++ MyBot.py
@@ -25,7 +25,7 @@ class MyBot(Player):
         Returns if existing and free the field. Sets the field to the given player_number.
 
                 Parameters:
-                        self: player
+                        self: bot
                         board: Board where Player is active
                         
 
@@ -64,6 +64,17 @@ class MyBot(Player):
         return super().make_move(row, col, board)
         
     def check_in_danger(self, board: Board) -> tuple:
+        '''
+        Checks if bot is in danger of being defeated. If thats the case he tries to stop the threatening row.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        
+
+                Returns:
+                        tuple: field coordinates
+        '''
         if self.player_number == 1:
             check_number = 2
         else:
@@ -79,6 +90,17 @@ class MyBot(Player):
         return check_danger
 
     def check_in_danger_horizontal(self, board: Board, check_number: int) -> tuple:
+        '''
+        Checks if bot is in danger of being defeated horizontally. If thats the case he tries to stop the threatening row.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        check_number: playernumber of the opponent 
+
+                Returns:
+                        tuple: field coordinates
+        '''
         for offset_horizontal in range(board.n_row):
             counter1 = 1   
             for offset_vertical in range (board.m_col - 1):
@@ -95,6 +117,17 @@ class MyBot(Player):
         return None
     
     def check_in_danger_vertical(self, board: Board, check_number: int) -> tuple:
+        '''
+        Checks if bot is in danger of being defeated vertically. If thats the case he tries to stop the threatening row.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        check_number: playernumber of the opponent 
+
+                Returns:
+                        tuple: field coordinates
+        '''
         for offset_vertical in range(board.m_col):
             counter1 = 1   
             for offset_horizontal in range (board.n_row-1):
@@ -111,6 +144,17 @@ class MyBot(Player):
         return None
     
     def check_in_danger_TLBR(self, board: Board, check_number: int) -> tuple:
+        '''
+        Checks if bot is in danger of being defeated diagonally (top left to bottom right). If thats the case he tries to stop the threatening row.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        check_number: playernumber of the opponent 
+
+                Returns:
+                        tuple: field coordinates
+        '''
         for offset_horizontal in range((0 - board.n_row), (board.n_row - (board.k_in_a_row - 1))):
             diagonal_array = np.diagonal(board.field, offset_horizontal)
             counter1 = 1 
@@ -126,6 +170,17 @@ class MyBot(Player):
         return None
     
     def check_in_danger_BLTR(self, board: Board, check_number: int) -> tuple:
+        '''
+        Checks if bot is in danger of being defeated diagonally (bottom left to top right). If thats the case he tries to stop the threatening row.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        check_number: playernumber of the opponent 
+
+                Returns:
+                        tuple: field coordinates
+        '''
         for offset_horizontal in range((0-board.n_row), (board.n_row -(board.k_in_a_row-1))):
             diagonal_array = np.diagonal(board.flipped_field, offset_horizontal)
             counter1 = 1 
@@ -141,18 +196,70 @@ class MyBot(Player):
         return None
     
     def from_flipped_to_non_flipped(self, flipped_col: int, board: Board) -> int:
-            return (board.m_col - flipped_col)
+        '''
+            Calculates the matching field of the flipped field for the normal field.
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        check_number: playernumber of the opponent 
+
+                Returns:
+                        tuple: field coordinates
+        '''
+        return (board.m_col - flipped_col)
 
     def make_defending_move_TLBR(self, counter: int, offset_horizontal: int, offset_absolute: int, offset_vertical: int, board: Board, length: int) -> tuple:
+        '''
+            Checks if given lenght of opponent row is reached (top left to bottom right). If thats the case he tries to stop the threatening row.
+                Parameters:
+                        self: bot
+                        counter: Lenght of the opposing row
+                        offset_horizontal: horizontal offset 
+                        offset_absolute: absolute value of the horizontal offset 
+                        offset_vertical: vertical 
+                        board: Board where Player is active
+                        length: Length row needs to reach
+
+                Returns:
+                        tuple: field coordinates
+        '''
         if counter == length and self.make_move_TLBR(offset_horizontal, offset_absolute, offset_vertical, board, length) != None:
             return self.make_move_TLBR(offset_horizontal, offset_absolute, offset_vertical, board, length)
         return None
     
     def make_defending_move_BLTR(self, counter: int, offset_horizontal: int, offset_absolute: int, offset_vertical: int, board: Board, length: int) -> tuple:
+        '''
+            Checks if given lenght of opponent row is reached (bottom left to top right). If thats the case he tries to stop the threatening row.
+                Parameters:
+                        self: bot
+                        counter: Lenght of the opposing row
+                        offset_horizontal: horizontal offset 
+                        offset_absolute: absolute value of the horizontal offset 
+                        offset_vertical: vertical offset
+                        board: Board where Player is active
+                        length: Length row needs to reach
+
+                Returns:
+                        tuple: field coordinates
+        '''
         if counter == length and self.make_move_BLTR(offset_horizontal, offset_absolute, offset_vertical, board, length) != None:
             return self.make_move_BLTR(offset_horizontal, offset_absolute, offset_vertical, board, length)
             
     def make_move_TLBR(self, offset_horizontal: int, offset_absolute: int, offset_vertical: int, board: Board, length: int) -> tuple:
+        '''
+            Calculates field where to make own move.
+            (top left to bottom right)
+                Parameters:
+                        self: bot
+                        offset_horizontal: horizontal offset 
+                        offset_absolute: absolute value of the horizontal offset 
+                        offset_vertical: vertical offset
+                        board: Board where Player is active
+                        length: Length row needs to reach
+
+                Returns:
+                        tuple: field coordinates
+        '''
         if offset_horizontal >= 0:
             if board.position_valid((offset_vertical+(1-length)), (offset_absolute-(length-2))):
                 return ((offset_vertical+(1-length)), (offset_absolute-(length-2)))
@@ -165,6 +272,20 @@ class MyBot(Player):
                 return ((offset_absolute+3), (offset_vertical+2))  
             
     def make_move_BLTR(self, offset_horizontal: int, offset_absolute: int, offset_vertical: int, board: Board, length: int) -> tuple:
+        '''
+            Calculates field where to make own move.
+            (bottom left to top right)
+                Parameters:
+                        self: bot
+                        offset_horizontal: horizontal offset 
+                        offset_absolute: absolute value of the horizontal offset 
+                        offset_vertical: vertical offset
+                        board: Board where Player is active
+                        length: Length row needs to reach
+
+                Returns:
+                        tuple: field coordinates
+        '''
         if offset_horizontal >= 0:
             if board.position_valid((offset_vertical+(1-length)), (self.from_flipped_to_non_flipped(offset_absolute-(length-2), board)-1)):
                 return ((offset_vertical+(1-length)), (self.from_flipped_to_non_flipped(offset_absolute-(length-2), board)-1))
@@ -177,6 +298,17 @@ class MyBot(Player):
                 return ((offset_absolute+3), (self.from_flipped_to_non_flipped(offset_vertical+2, board)-1))
 
     def finish_game(self, board: Board) -> tuple:
+        '''
+        Checks if bot has the chance to win. If thats the case he tries to finish the row.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        
+
+                Returns:
+                        tuple: field coordinates
+        '''
         check_number = self.player_number
         check_winning = None
         check_winning = self.check_in_danger_horizontal(board,check_number)
@@ -189,6 +321,17 @@ class MyBot(Player):
         return check_winning
     
     def start_attack(self, board: Board) -> tuple:
+        '''
+        Starts an attacking row of two. This will enable the finish_game method.
+
+                Parameters:
+                        self: bot
+                        board: Board where Player is active
+                        
+
+                Returns:
+                        tuple: field coordinates
+        '''
         for offset_horizontal in range (board.n_row - 1):
             for offset_vertical in range (board.m_col - 1):
                 if board.field[offset_horizontal, offset_vertical] == self.player_number:
